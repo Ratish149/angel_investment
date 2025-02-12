caf3dba2f34c09619ee4800eb4c9575570530c6c
Ratish149 <ratish.shakya149@gmail.com>
2025-02-12 12:20:30 +0545

    asd

--- angel_investment/settings.py
+++ angel_investment/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'angel_investment',
         'USER': 'ratish',
-        'PASSWORD': 'ratish',
+        'PASSWORD': 'ratish123',
         'HOST': 'localhost',
         'PORT': '',
     }
