a274f371f9b3e55e48b255df5ddbf1ea025e06ca
Ratish149 <ratish.shakya149@gmail.com>
2025-02-12 11:54:47 +0545

    asd

--- angel_investment/settings.py
+++ angel_investment/settings.py
@@ -77,10 +77,21 @@ WSGI_APPLICATION = 'angel_investment.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+} """
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'angel_investment',
+        'USER': 'ratish',
+        'PASSWORD': 'ratish123',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
