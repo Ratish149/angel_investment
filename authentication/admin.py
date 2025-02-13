844b7abb959262a0f63270f35c7fcc62e380edb9
Ratish149 <ratish.shakya149@gmail.com>
2025-02-13 16:18:57 +0545

    asda

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import CustomUser, Company, CompanyTag, CompanyTeam
 from unfold.admin import ModelAdmin
+from django.db import models
+from tinymce.widgets import TinyMCE
 # Register your models here.
 
 class CustomUserAdmin(ModelAdmin):
@@ -8,10 +10,14 @@ class CustomUserAdmin(ModelAdmin):
     list_filter = ('role', 'is_activated')
     search_fields = ('full_name','email','first_name')
 
+
 class CompanyAdmin(ModelAdmin):
     list_display = ('company_name', 'stage', 'contact_city', 'contact_country', 'contact_phone_number', 'contact_email', 'website', 'amount_raising', 'affiliation')
     list_filter = ('stage', 'contact_city', 'contact_country', 'amount_raising', 'affiliation')
     search_fields = ('company_name', 'contact_city', 'contact_country', 'contact_email', 'website', 'amount_raising', 'affiliation')
+    formfield_overrides = {
+        models.TextField: {'widget': TinyMCE,},
+    }
 
 class CompanyTagAdmin(ModelAdmin):
     list_display = ('name',)
