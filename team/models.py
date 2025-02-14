bf1b87c6c995deb5090663833ea3e505f3103410
Ratish149 <ratish.shakya149@gmail.com>
2025-02-14 17:29:46 +0545

    dasd

--- team/models.py
+++ team/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class OurTeam(models.Model):
     name = models.CharField(max_length=200)
-    role = models.CharField(max_length=200)
+    role = models.CharField(max_length=200,null=True,blank=True)
     description = models.TextField(null=True,blank=True)
     email= models.EmailField(null=True,blank=True)
     profile_picture = models.FileField(null=True,blank=True)
