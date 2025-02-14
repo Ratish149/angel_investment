d18008c5646cb5e316890ad98f985ad4e629d9b8
Ratish149 <ratish.shakya149@gmail.com>
2025-02-14 14:27:37 +0545

    dfd

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Event
+from unfold.admin import ModelAdmin
+from tinymce.widgets import TinyMCE
+from django.db import models
 # Register your models here.
+
+class TinyMce(ModelAdmin):
+    formfield_overrides = {
+        models.TextField: {'widget': TinyMCE,}
+    }
+
+admin.site.register(Event,TinyMce)
