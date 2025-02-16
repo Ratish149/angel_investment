474cd6b7cb8092c64a6f1efd44f878e63fd727f1
Ratish149 <ratish.shakya149@gmail.com>
2025-02-16 15:27:54 +0545

    asd

--- angel_investment/settings.py
+++ angel_investment/settings.py
@@ -94,14 +94,14 @@ WSGI_APPLICATION = 'angel_investment.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+} """
 
-""" DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'investly',
@@ -110,7 +110,7 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '',
     }
-} """
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
