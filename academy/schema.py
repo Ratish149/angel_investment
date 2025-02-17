8a48b2ae282c6c0e5bb2c83255fe62fde0623159
Ratish149 <ratish.shakya149@gmail.com>
2025-02-17 13:21:32 +0545

    asda

--- academy/schema.py
+++ academy/schema.py
@@ -10,8 +10,14 @@ class ArticleType(DjangoObjectType):
         filter_fields = ['academy__id']
 
 class AcademyType(DjangoObjectType):
+    total_articles = graphene.Int()
+
     class Meta:
         model = Academy
+        fields = ('id', 'title', 'logo', 'description', 'total_articles')
+
+    def resolve_total_articles(self, info):
+        return self.article_set.count()
 
 class Query(graphene.ObjectType):
     articles = DjangoPaginationConnectionField(ArticleType)
--- angel_investment/schema.py
+++ angel_investment/schema.py
@@ -61,8 +61,14 @@ class ArticleType(DjangoObjectType):
         filter_fields = ['academy__id']
 
 class AcademyType(DjangoObjectType):
+    total_articles = graphene.Int()
+
     class Meta:
         model = Academy
+        fields = ('id', 'title', 'logo', 'description', 'total_articles')
+
+    def resolve_total_articles(self, info):
+        return self.article_set.count()
 
 class CustomUserType(DjangoObjectType):
     class Meta:
