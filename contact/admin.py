1a8b0435e2592dcdddabce768abfdccefc968540
Ratish149 <ratish.shakya149@gmail.com>
2025-03-09 18:01:38 +0545

    dfd

--- contact/admin.py
+++ contact/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
 from .models import Contact, Newsletter
 # Register your models here.
 
-admin.site.register(Contact)
-admin.site.register(Newsletter)
+admin.site.register(Contact, ModelAdmin)
+admin.site.register(Newsletter, ModelAdmin)
