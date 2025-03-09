64f8f012fc8a23db976cbcd76abfde961edf3ce3
Ratish149 <ratish.shakya149@gmail.com>
2025-03-09 17:55:00 +0545

    dfd

--- contact/urls.py
+++ contact/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import ContactView
+from .views import ContactView, NewsletterView
 urlpatterns = [
     path('contact/', ContactView.as_view()),
+    path('newsletter/', NewsletterView.as_view()),
 ]
