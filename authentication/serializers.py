37f5688810eaf3446aafb1534e66a90d5b2ace7e
Ratish149 <ratish.shakya149@gmail.com>
2025-03-16 16:20:19 +0545

    dfd

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -5,7 +5,7 @@ from .models import CustomUser, Company, CompanyTag, CompanyTeam, Users
 class UsersSerializer(serializers.ModelSerializer):
     class Meta:
         model = Users
-        fields = ['id', 'full_name', 'email', 'role', 'organization_name','organization_logo', 'organization_description', 
+        fields = ['id', 'full_name', 'email', 'role', 'organization_role','organization_name','organization_logo', 'organization_description', 
                  'contact_number', 'website_link', 'document']
     
 class CustomUserSerializer(serializers.ModelSerializer):
