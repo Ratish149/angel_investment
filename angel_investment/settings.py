0d0aee2fc3172159f6e398c6134748f4aa2a06df
Ratish149 <ratish.shakya149@gmail.com>
2025-07-21 10:55:35 +0545

    dfd

--- angel_investment/settings.py
+++ angel_investment/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -109,9 +113,9 @@ WSGI_APPLICATION = 'angel_investment.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'investly',
-        'USER': 'ratish',
-        'PASSWORD': 'ratish123',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': 'localhost',
         'PORT': '',
     }
