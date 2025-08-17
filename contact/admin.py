1684a375ae9b55846376011ff3acaf67cba64d29
Ratish149 <ratish.shakya149@gmail.com>
2025-08-17 16:37:04 +0545

    dfd

--- contact/admin.py
+++ contact/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from unfold.admin import ModelAdmin
-from .models import Contact, Newsletter
+from .models import Contact, Newsletter, BookSpace
 # Register your models here.
+
+
+class BookSpaceAdmin(ModelAdmin):
+    list_display = ('name', 'email', 'phone_number',
+                    'date_and_time',)
+    list_filter = ('date_and_time',)
+    search_fields = ('name', 'email', 'phone_number')
+    ordering = ('-created_at',)
+
 
 admin.site.register(Contact, ModelAdmin)
 admin.site.register(Newsletter, ModelAdmin)
+admin.site.register(BookSpace, BookSpaceAdmin)
